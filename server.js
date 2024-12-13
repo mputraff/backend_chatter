e37ab70574c0e7833c0b0f4b94a57ee14fa9c2d9
putrafauzan32@gmail.com <email@example.com>
2024-12-13 19:16:50 +0700

    update api

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.use(cors({
   origin : '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
 }));
 
 const startServer = async () => {
